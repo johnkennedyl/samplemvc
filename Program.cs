eadc1ff95ecb6f1d43e266391d8b975bd76e0ce3
john.kennedy <john.kennedy@icarasia.com>
2019-07-12 17:51:20 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Samplemvc1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
